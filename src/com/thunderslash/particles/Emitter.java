128477085bfa9bdeb9471426c58592984e4011b7
Heikki Heiskanen <heikkiheiskanen@hotmail.com>
2018-01-05 15:27:56 +0200

    Naive particles implementation.

---
+++ src/com/thunderslash/particles/Emitter.java
@@ -0,0 +1,76 @@
+package com.thunderslash.particles;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.thunderslash.engine.Game;
+import com.thunderslash.enumerations.Direction;
+import com.thunderslash.utilities.SpriteCreator;
+
+public class Emitter {
+
+    private int x, y;
+    private List<Particle> particles;
+    
+    public Emitter(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.particles = new ArrayList<Particle>();
+    
+        SpriteCreator sc = Game.instance.getSpriteCreator();
+        BufferedImage img = sc.CreateCustomSizeSprite(20, 6 * 32, 5, 5);
+        
+        // preload n amount of particles
+        for(int i = 0; i < 20; i++) {
+            this.particles.add(new Particle(this.x, this.y, img));
+        }
+    }
+    
+    public void emit(int n, Direction dir) {
+        
+        // TODO: add more particles if no particles are present
+        if(n > this.particles.size()) n = this.particles.size();
+        
+        // acceleration direction
+        int ax = 0, ay = 0;
+        
+        switch(dir) {
+            case EAST:
+                ax = 1;
+                break;
+            case NORTH:
+                ay = -1;
+                break;
+            case SOUTH:
+                ay = 1;
+                break;
+            case WEST:
+                ax = -1;
+                break;
+                
+            case NORTH_EAST:
+                ax = 1;
+                ay = -1;
+                break;
+            case NORTH_WEST:
+                ax = -1;
+                ay = -1;
+                break;
+            case SOUTH_EAST:
+                ax = 1;
+                ay = 1;
+                break;
+            case SOUTH_WEST:
+                ax = -1;
+                ay = 1;
+                break;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            Particle current = this.particles.get(i);
+            current.setVelocity(ax, ay);
+            current.enable();
+        }
+    }
+}
---
+++ src/com/thunderslash/particles/Particle.java
@@ -0,0 +1,80 @@
+package com.thunderslash.particles;
+
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+import com.thunderslash.engine.Game;
+
+public class Particle {
+
+    private Point position = new Point(0, 0);
+    private Point velocity = new Point(0, 0);
+    private Point acceleration = new Point(0, 0);
+    
+    private BufferedImage img;
+    private double currentLifeTime = 0.0;
+    private double maxLifeTime = 10.0;
+    
+    private boolean enabled = false;
+    
+    public Particle(int x, int y, BufferedImage img) {
+        this.position.x = x;
+        this.position.y = y;
+        this.img = img;
+    }
+
+    public void enable() {
+        this.enabled = true;
+        this.currentLifeTime = 0.0;
+    }
+    
+    public void tick() {
+        
+        if(this.enabled == false) return;
+        
+        if(this.currentLifeTime < maxLifeTime) {
+            
+            this.currentLifeTime += Game.instance.getTimeBetweenFrames();
+            
+            this.velocity.x += this.acceleration.x;
+            this.velocity.y += this.acceleration.y + Game.GRAVITY;
+            
+            this.position.x += velocity.x;
+            this.position.y += velocity.y;
+            
+        } else {
+            
+            this.enabled = false;
+            
+        }
+    }
+    
+    public void render(Graphics g) {
+        if(this.enabled) g.drawImage(this.img, this.position.x, this.position.y, null);       
+    }
+    
+    public int getX() { return this.position.x; }
+    public void setX(int x) { this.position.x = x; }
+    public int getY() { return this.position.y; }
+    public void setY(int y) { this.position.y = y; }
+    public BufferedImage getImg() { return img; }
+    public void setImg(BufferedImage img) { this.img = img; }
+    public Point getAcceleration() { return acceleration; }
+    public void setAcceleration(Point acceleration) { this.acceleration = acceleration; }
+    public Point getVelocity() { return velocity; }
+    public void setVelocity(Point velocity) { this.velocity = velocity; }
+
+    public void setVelocity(int ax, int ay) {
+        this.velocity.x = ax;
+        this.velocity.y = ay;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
